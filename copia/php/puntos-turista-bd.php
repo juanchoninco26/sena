8691fc3717beeb41b92854253d0382c927513fda
juanchoninco26 <96754383+juanchoninco26@users.noreply.github.com>
2022-04-29 17:48:22 -0500

    Create README.md

---
+++ copia/php/puntos-turista-bd.php
@@ -0,0 +1,17 @@
+<?php
+//ver video:https://www.youtube.com/watch?v=pn2v9lPakHQ
+$ared = new mysqli(
+    "localhost",
+    "root",
+    "",
+    "ared",
+    2526
+);
+
+$ared -> set_charset("utf8");
+/*if (isset($ared)){
+    echo 'bien';
+}else{
+    echo 'mal';
+}*/
+?>
