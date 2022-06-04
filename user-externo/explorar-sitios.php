8edfc877d211222bfe142b98ab2ee017ec9d6c2c
juanchoninco26 <juanchoninco26@gmail.com>
2022-06-03 20:45:03 -0500

    main

--- user-externo/explorar-sitios.php
+++ user-externo/explorar-sitios.php
@@ -9,11 +9,67 @@
     <title>Document</title>
 </head>
 <body>
-<div class="">
+<div class="main">
     <?php 
     include '/xampp/xampp/htdocs/360/ruta360/php/include/men-user-extern.php';
     ?>
     </div>
-
+    <div class="block">
+    <div class="user">
+        <div class="usercaja">
+         <div class="info">
+           <div class="usuario">
+             <h3>usuario</h3>
+             <p>nombre</p>
+           </div>
+           <div class="foto">
+             <img src="/imagenes/user.svg" alt="">
+           </div>
+         </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="subcontainer">
+            <p><strong>sitios</strong> locales</p>
+            <div class="imagenes">
+                <div>
+                    <img src="/imagenes/cascada.jpg" alt="" width="90px" height="96px">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+    <div class="subcontainer">
+            <p><strong>sitios</strong> locales</p>
+            <div class="imagenes">
+                <div>
+                    <img src="/imagenes/cascada.jpg" alt="" width="90px" height="96px">
+                </div>
+            </div>
+        </div>
+        </div>
+    <div class="container">
+    <div class="subcontainer">
+            <p><strong>sitios</strong> locales</p>
+            <div class="imagenes">
+                <div>
+                    <img src="/imagenes/cascada.jpg" alt="" width="90px" height="96px">
+                </div>
+                <div>
+                    <img src="/imagenes/cascada.jpg" alt="" width="90px" height="96px">
+                </div>
+                <div>
+                    <img src="/imagenes/cascada.jpg" alt="" width="90px" height="96px">
+                </div>
+                <div>
+                    <img src="/imagenes/cascada.jpg" alt="" width="90px" height="96px">
+                </div>
+            </div>
+            <div>
+                <img src="/imagenes/derecha.svg" alt="">
+            </div>
+        </div>
+    </div>
+    </div>
 </body>
 </html>
