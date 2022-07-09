6b7571872fcaa04d1282963dfcabeb9a6797e546
juanchoninco26 <juanchoninco26@gmail.com>
2022-07-09 16:03:50 -0500

    actualizar

--- php/correo.php
+++ php/correo.php
@@ -8,34 +8,23 @@ if (isset($_POST['boton'])) {
  //validar correo existente     
 echo (filter_var($correo[1], FILTER_VALIDATE_EMAIL) ? 'Es email' : 'No es email').PHP_EOL;
 
- //correo activo
-  $boton = 1;
+ //id del correo
+  $email  = 'priyank@geeks.com';
+  $username = strstr($email, '@', true);
+  echo $username."\n";
+
+ $boton=1;
 
   //insertar datos en la tabla
   $lista = "INSERT INTO Correo (Id_correo, correo, Activo, Id_turista) 
-  VALUES ('','$correo','$boton',NULL)";
+  VALUES ('$username','$correo','$boton',NULL)";
   $resultado = mysqli_query($ared,$lista) or die("error:". mysqli_error($ared));
 
   //comprobamos que el resulatado sea correcto para recargar la pagina con header 
   if ($resultado) {
+    header("location:../Index.php");
 
-    header("location:.../Index.php");
-=======
-<<<<<<< HEAD
-    header("location:../Index.php");
-=======
-<<<<<<< HEAD
-    header("location:../Index.php");
-=======
-<<<<<<< HEAD
-    header("location:../Index.php");
-=======
-    header("location:/Trabajo Sena/SENA/Index.php");
->>>>>>> e25be13d660ca10dee4791c05b8a23b5f7f7ee74
->>>>>>> d9d0045b7a126d1d3159cbc59e2e40dbbc0100d9
->>>>>>> 12af2e34aa95221cb6e9e31341951a9a438141f8
->>>>>>> f48882082773e3a22af3e8ba6fed53ad7d8effa0
-}else {
+} else {
     echo 'mal';
 }
 }
--- php/eliminarlugar.php
+++ php/eliminarlugar.php
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
-<?php include '../php/puntos-turista-bd.php';
-=======
-<<<<<<< HEAD
-<?php include '../php/puntos-turista-bd.php';
-=======
-<<<<<<< HEAD
-<?php include '../php/puntos-turista-bd.php';
-=======
-<<<<<<< HEAD
-<?php include '../php/puntos-turista-bd.php';
-=======
-<?php include '/xampp/htdocs/Trabajo Sena/SENA/php/bd.php';
->>>>>>> e25be13d660ca10dee4791c05b8a23b5f7f7ee74
->>>>>>> d9d0045b7a126d1d3159cbc59e2e40dbbc0100d9
->>>>>>> 12af2e34aa95221cb6e9e31341951a9a438141f8
->>>>>>> f48882082773e3a22af3e8ba6fed53ad7d8effa0
+<?php include './php/puntos-turista-bd.php';
+
 //eliminar paquete
 if (isset($_GET['Cod_paquete'])) {
     $id=$_GET['Cod_paquete'];
@@ -22,25 +7,8 @@ if (isset($_GET['Cod_paquete'])) {
     $elim= "DELETE FROM paquetes_turisticos  WHERE Cod_paquete ='$cod'";
     $resultado = mysqli_query($ared,$elim)or die("error:". mysqli_error($ared));
 
-<<<<<<< HEAD
     header('Location: ../PaginaPrincipal/Pago.php');
-=======
-<<<<<<< HEAD
-    header('Location: ../PaginaPrincipal/Pago.php');
-=======
-<<<<<<< HEAD
-    header('Location: ../PaginaPrincipal/Pago.php');
-=======
-<<<<<<< HEAD
-    header('Location: ../PaginaPrincipal/Pago.php');
-=======
-    header('Location: /Trabajo Sena/SENA/PaginaPrincipal/Pago.php');
->>>>>>> e25be13d660ca10dee4791c05b8a23b5f7f7ee74
->>>>>>> d9d0045b7a126d1d3159cbc59e2e40dbbc0100d9
->>>>>>> 12af2e34aa95221cb6e9e31341951a9a438141f8
->>>>>>> f48882082773e3a22af3e8ba6fed53ad7d8effa0
-    }
-        
+    }  
      else {
         echo "mal";
     }
