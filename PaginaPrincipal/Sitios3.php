a8509adc33dd908f9cdb0710aa94a9897e6910d1
juanchoninco26 <juanchoninco26@gmail.com>
2022-07-21 21:43:15 -0500

    arreglo url

--- PaginaPrincipal/Sitios3.php
+++ PaginaPrincipal/Sitios3.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600|Open+Sans" rel="stylesheet">
-    <link rel="stylesheet" href="/Trabajo Sena/SENA/Estilos/EstilosSitios3.css">
+    <link rel="stylesheet" href="../Estilos/EstilosSitios3.css">
 </head>
 <body>
 <body background="../imagenes/MiradorCacica2.png" bgcolor="aqua"></body>
