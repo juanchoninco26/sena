092e6b9f93b8e013b56e86c81261d2280dbb2206
juan ninco <juanchoninco26@gmail.com>
2023-01-07 10:15:00 -0500

    json calendario

--- usuarios/gerente/reservaciones.php
+++ usuarios/gerente/reservaciones.php
@@ -13,9 +13,8 @@ include '../../php/puntos-turista-bd.php';
     <link rel="stylesheet" type="text/css" href="/estilos/reservaciones.css">
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdn.jsdelivr.net/npm/fullcalendar-scheduler@6.0.2/index.global.min.js"></script>
-    <!-- <script src='/fullcalendar/dist/index.global.js'></script>
-    <script src='/fullcalendar/dist/index.global.min.js'></script> -->
+    <script src="../../fullcalendar/dist/index.global.js"></script>
+    <script src="/fullcalendar/packages/core/locales/es-us.global.js"></script>
 
     <title>reservaciones</title>
 </head>
